38d62985901c76366226f3a4217ecf70eb6343ff
Keks <keks@htmlacademy.ru>
2022-02-26 23:23:38 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -2,22 +2,47 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random - ссылка на функцию
 function getRandomIntInclusive(min, max) {
 
+  if (min > max) {
+    const numberConteiner = min;
+    min = max;
+    max = numberConteiner;
+  }
+
+  if (min === max) {
+    return max;
+  }
+
   if ((min >= 0 && max > 0) && (min < max)) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  console.log('Значение min не может быть больше max или меньше нуля');
+  return NaN;
 }
+
+getRandomIntInclusive(1, 3);
 
 
 function getRandomLocationPoint(min, max, sign) {
 
-  if ((min >= 0 && max > 0) && (min < max)) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return (Math.floor(Math.random() * (max - min + 1)) + min)+Number(Math.random().toFixed(sign));
+  if (min > max) {
+    const numberConteiner = min;
+    min = max;
+    max = numberConteiner;
   }
+
+  if (min === max) {
+    return max;
+  }
+
+  if (sign < 0) {
+    sign *= -1;
+  }
+
+  if ((min >= 0 && max > 0) && (min < max)) {
+    return (Math.floor(Math.random() * (max - min)) + min)+Number(Math.random().toFixed(sign));
+  }
+  return NaN;
 }
 
-
+getRandomLocationPoint(1, 4, 5);
