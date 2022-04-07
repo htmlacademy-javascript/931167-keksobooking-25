a125018b38fbd5389f71553ec81878ea200a1500
Dmitry <free-doom46@mail.ru>
2022-04-08 02:59:03 +0300

    adds template

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-import {createUserCard} from './mocks.js';
+import {makeAdverts} from './mocks.js';
+import {OFFERS_COUNT} from './data.js';
+import {similarCard} from './template.js';
+import {inactiveStateForm} from './form.js';
 
-// console.log(createUserCard);
+
--- js/mocks.js
+++ js/mocks.js
@@ -1,4 +1,4 @@
-import {OFFERS_COUNT, OFFER_TITLES, OFFER_TYPES, OFFER_CHECKINS, OFFER_CHECKOUTS, OFFER_FEATURES, OFFER_DESCRIPTIONS, OFFER_PHOTOS, OFFER_LOCATION_MIN_LAT, OFFER_LOCATION_MAX_LAT, OFFER_LOCATION_MIN_LNG,OFFER_LOCATION_MAX_LNG, OFFER_LOCATION_PRECISION} from './data.js';
+import {OFFER_TITLES, OFFER_TYPES, OFFER_CHECKINS, OFFER_CHECKOUTS, OFFER_FEATURES, OFFER_DESCRIPTIONS, OFFER_PHOTOS, OFFER_LOCATION_MIN_LAT, OFFER_LOCATION_MAX_LAT, OFFER_LOCATION_MIN_LNG,OFFER_LOCATION_MAX_LNG, OFFER_LOCATION_PRECISION} from './data.js';
 
 import {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement, getRandomArrayElements, leadingZero} from './util.js'
 
@@ -19,30 +19,24 @@ const generateAuthorId = generateId();
 
 const makeAuthor = (userId) => `img/avatars/user${leadingZero(userId)}.png`;
 
-const makeOffers = (count) => {
-  const conteiner = [];
-  for (let i = 1; i <= count; i++) {
-    conteiner.push({
-      author: makeAuthor(generateAuthorId()),
-      offer: {
-        title: getRandomArrayElement(OFFER_TITLES),
-        address: makeLocation(),
-        price: getRandomPositiveInteger(0, 100),
-        type: getRandomArrayElement(OFFER_TYPES),
-        rooms: getRandomPositiveInteger(0, 10),
-        guests: getRandomPositiveInteger(0, 10),
-        checkin: getRandomArrayElement(OFFER_CHECKINS),
-        checkout: getRandomArrayElement(OFFER_CHECKOUTS),
-        features: getRandomArrayElements(OFFER_FEATURES),
-        description: getRandomArrayElement(OFFER_DESCRIPTIONS),
-        photos: getRandomArrayElement(OFFER_PHOTOS)
-      },
-      location: makeLocation()
-    });
-  }
-  return conteiner;
-};
+const makeAdvert = () => ({
+  author: makeAuthor(generateAuthorId()),
+  offer: {
+    title: getRandomArrayElement(OFFER_TITLES),
+    address: Object.values(makeLocation()).join(', '),
+    price: getRandomPositiveInteger(0, 100),
+    type: getRandomArrayElement(OFFER_TYPES),
+    rooms: getRandomPositiveInteger(0, 10),
+    guests: getRandomPositiveInteger(0, 10),
+    checkin: getRandomArrayElement(OFFER_CHECKINS),
+    checkout: getRandomArrayElement(OFFER_CHECKOUTS),
+    features: getRandomArrayElements(OFFER_FEATURES),
+    description: getRandomArrayElement(OFFER_DESCRIPTIONS),
+    photos: getRandomArrayElements(OFFER_PHOTOS)
+  },
+  location: makeLocation()
+});
 
-const createUserCard =  makeOffers(OFFERS_COUNT);
+const makeAdverts = (count) => Array.from({length: count}, makeAdvert);
 
-export {createUserCard};
+export {makeAdverts};
