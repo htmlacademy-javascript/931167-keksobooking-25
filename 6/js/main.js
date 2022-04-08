2fa7a060e2c756f6dff50273d6c5f36fb5d419cf
Keks <keks@htmlacademy.ru>
2022-04-08 22:07:07 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -2,4 +2,5 @@ import {makeAdverts} from './mocks.js';
 import {OFFERS_COUNT} from './data.js';
 import {similarCard} from './template.js';
 
-similarCard(makeAdverts(OFFERS_COUNT)[0]);
+const mapCanvas = document.querySelector('#map-canvas');
+mapCanvas.append(similarCard(makeAdverts(OFFERS_COUNT)[0]));
